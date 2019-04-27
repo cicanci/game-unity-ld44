0a2fc0cf61a534fb749a18927ef4746caa26f702
Bruno Cicanci <brunocicanci@gmail.com>
2019-04-27 17:52:38 -0300

    Adjuested detault stats values

--- Assets/Scripts/Character/CharacterEnemy.cs
+++ Assets/Scripts/Character/CharacterEnemy.cs
@@ -1,4 +1,6 @@
-﻿using UnityEngine;
+﻿using System;
+using System.Collections;
+using UnityEngine;
 
 public class CharacterEnemy : Character
 {
@@ -24,5 +26,16 @@ public class CharacterEnemy : Character
         Debug.LogFormat("[HP: {0} AT: {1} DF: {2}] {3} took {4} damage",
             _characterStats.Life, _characterStats.Attack, _characterStats.Defense, name, damage);
 #endif
+
+        if(_characterStats.Life <= 0)
+        {
+            StartCoroutine(EnemyKilled());
+        }
+    }
+
+    private IEnumerator EnemyKilled()
+    {
+        yield return new WaitForSeconds(1);
+        Destroy(gameObject);
     }
 }
