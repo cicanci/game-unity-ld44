03079cf6caa7da957a6b1d5e2320762c2663caf5
Bruno Cicanci <brunocicanci@gmail.com>
2019-04-27 14:36:05 -0300

    Attack and defense reflecting life

--- Assets/Scripts/Character.cs
+++ Assets/Scripts/Character.cs
@@ -5,16 +5,23 @@ using UnityEngine.UI;
 public class Character : MonoBehaviour
 {
     [SerializeField]
-    private Text _characterLifeText = null;
+    private DefaultStats _defaultStats = null;
 
     [SerializeField]
-    private DefaultStats _defaultStats = null;
+    private Text _lifeText = null;
+
+    [SerializeField]
+    private Text _attackText = null;
+
+    [SerializeField]
+    private Text _defenseText = null;
 
     private CharacterStats _characterStats;
 
     private void Start()
     {
         _characterStats = new CharacterStats(_defaultStats);
+        UpdateCharacterInfo();
     }
 
     public void IncreaseAttribute(AttributeType type)
@@ -36,6 +43,8 @@ public class Character : MonoBehaviour
 
     private void UpdateCharacterInfo()
     {
-        _characterLifeText.text = string.Format("HP: {0}", _characterStats.Life);
+        _lifeText.text = string.Format("HP: {0}", _characterStats.Life);
+        _attackText.text = string.Format("AT: {0}", _characterStats.Attack);
+        _defenseText.text = string.Format("DF: {0}", _characterStats.Defense);
     }
 }
