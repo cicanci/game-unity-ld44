7410df4e8c77984ce6b01e863c7cac47be4faca2
Bruno Cicanci <brunocicanci@gmail.com>
2019-04-27 18:17:35 -0300

    Fixed namespace

--- Assets/Scripts/UI/CameraShake.cs
+++ Assets/Scripts/UI/CameraShake.cs
@@ -1,73 +1,70 @@
 ﻿using System.Collections;
 using UnityEngine;
 
-namespace Asteroids.UI
+public class CameraShake : MonoBehaviour
 {
-    public class CameraShake : MonoBehaviour
+    [SerializeField]
+    private float _shakeIntensity = 0;
+
+    [SerializeField]
+    private float _playerShakeDuration = 0;
+
+    [SerializeField]
+    private float _enemyShakeDuration = 0;
+
+    private float _shakeDuration;
+    private Vector3 _originalPos;
+    private Coroutine _shakeCameraCoroutine;
+
+    private void Start()
     {
-        [SerializeField]
-        private float _shakeIntensity = 0;
+        _shakeDuration = 0;
+        _originalPos = transform.localPosition;
+    }
 
-        [SerializeField]
-        private float _playerShakeDuration = 0;
+    private void OnEnable()
+    {
+        MessageManager.Instance.AddListener<PlayerAttackMessage>(OnPlayerAttackMessage);
+        MessageManager.Instance.AddListener<EnemyAttackMessage>(OnEnemyAttackMessage);
+    }
 
-        [SerializeField]
-        private float _enemyShakeDuration = 0;
+    private void OnDisable()
+    {
+        MessageManager.Instance.RemoveListener<PlayerAttackMessage>(OnPlayerAttackMessage);
+        MessageManager.Instance.RemoveListener<EnemyAttackMessage>(OnEnemyAttackMessage);
+    }
 
-        private float _shakeDuration;
-        private Vector3 _originalPos;
-        private Coroutine _shakeCameraCoroutine;
-
-        private void Start()
+    private void ShakeCamera()
+    {
+        if (_shakeCameraCoroutine != null)
         {
-            _shakeDuration = 0;
-            _originalPos = transform.localPosition;
+            StopCoroutine(_shakeCameraCoroutine);
+        }
+        _shakeCameraCoroutine = StartCoroutine(ShakeCameraCoroutine());
+    }
+
+    private IEnumerator ShakeCameraCoroutine()
+    {
+        while (_shakeDuration > 0)
+        {
+            yield return null;
+            transform.localPosition = _originalPos + Random.insideUnitSphere * _shakeIntensity;
+            _shakeDuration -= Time.deltaTime;
         }
 
-        private void OnEnable()
-        {
-            MessageManager.Instance.AddListener<PlayerAttackMessage>(OnPlayerAttackMessage);
-            MessageManager.Instance.AddListener<EnemyAttackMessage>(OnEnemyAttackMessage);
-        }
+        _shakeDuration = 0;
+        transform.localPosition = _originalPos;
+    }
 
-        private void OnDisable()
-        {
-            MessageManager.Instance.RemoveListener<PlayerAttackMessage>(OnPlayerAttackMessage);
-            MessageManager.Instance.RemoveListener<EnemyAttackMessage>(OnEnemyAttackMessage);
-        }
+    private void OnPlayerAttackMessage(PlayerAttackMessage message)
+    {
+        _shakeDuration = _playerShakeDuration;
+        ShakeCamera();
+    }
 
-        private void ShakeCamera()
-        {
-            if(_shakeCameraCoroutine != null)
-            {
-                StopCoroutine(_shakeCameraCoroutine);
-            }
-            _shakeCameraCoroutine = StartCoroutine(ShakeCameraCoroutine());
-        }
-        
-        private IEnumerator ShakeCameraCoroutine()
-        {
-            while(_shakeDuration > 0)
-            {
-                yield return null;
-                transform.localPosition = _originalPos + Random.insideUnitSphere * _shakeIntensity;
-                _shakeDuration -= Time.deltaTime;
-            }
-
-            _shakeDuration = 0;
-            transform.localPosition = _originalPos;
-        }
-
-        private void OnPlayerAttackMessage(PlayerAttackMessage message)
-        {
-            _shakeDuration = _playerShakeDuration;
-            ShakeCamera();
-        }
-
-        private void OnEnemyAttackMessage(EnemyAttackMessage message)
-        {
-            _shakeDuration = _enemyShakeDuration;
-            ShakeCamera();
-        }
+    private void OnEnemyAttackMessage(EnemyAttackMessage message)
+    {
+        _shakeDuration = _enemyShakeDuration;
+        ShakeCamera();
     }
 }
